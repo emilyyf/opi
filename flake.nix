65bee3bc2192d9dd4683fdb6e95d02222b026d94
Emily <emily@anaboth.com>
2024-06-09 10:35:58 -0300

    chore: change to a flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Flake for building";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+						elixir
+            zsh
+          ];
+
+          shellHook = ''
+            exec zsh
+          '';
+        };
+      }
+    );
+}
